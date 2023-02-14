5ac310f9fa25f2a14d92b0475c3ab903d28be44e
chaemina <chaemina82@naver.com>
2023-02-14 16:52:38 +0900

    java script

---
+++ day2/6-11.js
@@ -0,0 +1,14 @@
+console.log("논리연산")
+
+let x = true;
+let y= false;
+
+console.log("x =" +x+ "y="+y);
+console.log("x&&y ="+ x&&y);
+console.log("x||y =" + x||y);
+console.log("!x=" +!x);
+
+
+
+console.log("(3>2) && (3<4) : "+(3>2) && (3<4) );
+console.log("(3==2)||(-1<0) : "+ (3==2)||(-1<0) );
